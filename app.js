7394687befefd08967dfd9f4bd951cd899ffcdbc
Justkim666 <tranhoangkim010702@gmail.com>
2024-02-23 15:35:29 +0700

    cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const contactsRouter = require("./app/routes/contact.route.js");
-const ApiError = require("./app/api-error");
+const ApiError = require("./app/api-error.js");
 const app = express();
 
 app.use(cors());
